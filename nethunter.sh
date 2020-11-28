e3fcbc5ab52d024d812f21e8825ccc42d1b5a921
BacktrackD3V <74622199+BacktrackD3V@users.noreply.github.com>
2020-11-28 19:03:04 +0300

    Update nethunter.sh

--- nethunter.sh
+++ nethunter.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
+clear
+echo Installing depends...
+apt install figlet -y
+apt install tar -y
+apt install proot -y
+apt install wget -y
+clear
+figlet Termux NetHunter
+echo By The BACKTRACK
+echo Downloading Rootfs...
 wget build.nethunter.com/kalifs/kalifs-latest/kalifs-arm64-minimal.tar.xz
-echo Decompressing Rootfs...
+echo Decompressing Rootfs... You can ignore mknod errors
 tar xJf kalifs-arm64-minimal.tar.xz
 rm -rf kalifs-arm64-minimal.tar.xz
 echo Configuring NetHunter...
-unset LD_PRELOAD
 touch start-nh.sh
 chmod +x start-nh.sh
 echo "#!/bin/sh" >> ~/start-nh.sh
